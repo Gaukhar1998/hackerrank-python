a517d0bb9279725384ea16bda0393ecc0d1b8df8
Slamova Gaukhar <11d.gaukhars@gmail.com>
2018-03-28 19:56:52 +0600

    Create What's Your Name.py

---
+++ Strings/What's Your Name.py
@@ -0,0 +1,7 @@
+def print_full_name(a, b):
+    print("Hello "+a+" "+b+"! You just delved into python.")
+    
+#another solution
+s = input()
+e = input()
+print("Hello %s %s! You just delved into python." %(s,e))
